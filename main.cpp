30bda53c3506a210163f709518b1ed472ee9acba
rotarymars <s.goto2050@gmail.com>
2024-11-22 16:40:59 +0900

    new header: touch

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
-#include <chrono>
-#include <colorsensor.hpp>
 #include <iostream>
 #include <motor.hpp>
-#include <thread>
+#include <colorsensor.hpp>
+/*#define _DEBUG*/
+int default_speed = 140, white_threshold = 100, black_threshold = 50; // TODO: FIX NUMBERS
+long double default_proportion = 1.1, default_i = 0.00055, default_d = 15;
+lmotor LMOTOR('C'), RMOTOR('D');
+  colorsensor LCOLOR(1), RCOLOR(2);
 int main() {
-  colorsensor leftcolor(1), rightcolor(2);
-  lmotor leftmotor('C'), rightmotor('D');
-  for (int i = 0; i < 1000; i++)
-    leftmotor.run(i);
-  leftmotor.brake();
-  for (int i = 0; i < 1000; i++)
-    rightmotor.run(i);
-  rightmotor.brake();
+
   return 0;
 }
